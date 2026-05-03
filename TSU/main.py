3e10b2bfc77c5842404a31cb45be986c1213bc4f
Stein Vandenbroeke <stein.vandenbroeke@student.uantwerpen.be>
2026-05-03 13:42:16 +0200

    change connection

--- TSU/main.py
+++ TSU/main.py
@@ -9,7 +9,7 @@ MQTT_PORT = 1883
 MQTT_TOPIC = "sensors/pycom/#"  # The '#' wildcard listens to all sub-topics
 
 # Your Digital Ocean WebSocket endpoint
-WS_URL = "ws://racesense.dcsteen.com:6969/ws"
+WS_URL = "ws://racesense.dcsteen.com:8000/ws/coral"
 
 # Initialize WebSocket connection
 ws = websocket.WebSocket()
