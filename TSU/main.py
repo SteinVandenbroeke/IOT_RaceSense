01d70f32b6e5f2039712f2d1eb8ef8aee6834688
NielsVandenBroeck <nielsvandenbroeck11@gmail.com>
2026-05-16 19:51:51 +0200

    other camera

--- TSU/main.py
+++ TSU/main.py
@@ -22,11 +22,11 @@ def camera_worker():
 
     print("Initializing Camera...")
     # NOTE: If this gives you a white screen, change `0` to `gstreamer_pipeline, cv2.CAP_GSTREAMER`
-    camera = cv2.VideoCapture('/dev/video1', cv2.CAP_V4L2)
+    camera = cv2.VideoCapture(1, cv2.CAP_V4L2)
 
     # Optional: Lower resolution to save bandwidth
-    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 320)
+    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
     while True:
         success, frame = camera.read()
